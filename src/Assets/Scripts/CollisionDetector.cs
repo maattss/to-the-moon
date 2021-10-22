8873d41bae37e97937b67162197dd605a37fee78
Nicolas Fløysvik <nicolas.floysvik@bouvet.no>
2021-10-23 01:24:02 +0200

    Updated main scene

--- src/Assets/Scripts/CollisionDetector.cs
+++ src/Assets/Scripts/CollisionDetector.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CollisionDetector : MonoBehaviour
 {
     public CollisionType Type = CollisionType.None;
+    public bool Remove = true;
+    Vector3 vec;
     void OnCollisionEnter(Collision collision)
     {
         if (Type == CollisionType.GameOver)
@@ -12,6 +14,20 @@ public class CollisionDetector : MonoBehaviour
         else if (Type == CollisionType.Refule)
             collision.gameObject.GetComponent<Movement>().fuelLevel = 100;
         Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.realtimeSinceStartup < 4) return;
+        vec = transform.position;
+        vec.y -= Time.deltaTime * 10;
+        transform.position = vec;
+
+        if (vec.y < -20 && Remove)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     public enum CollisionType
