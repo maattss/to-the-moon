0d4a4d82ad9cd16f2aff722473764fe85e432094
Nilhenrik <48207585+Nilhenrik@users.noreply.github.com>
2021-10-23 09:53:08 +0200

    merge

--- src/Assets/Scripts/Fuelbar.cs
+++ src/Assets/Scripts/Fuelbar.cs
@@ -5,12 +5,18 @@ public class Fuelbar : MonoBehaviour
 {
     [SerializeField]
     private Slider slider;
-
+    [SerializeField]
+    private Image sliderBar;
     public Movement Movement;
+
 
     // Update is called once per frame
     void Update()
     {
         slider.value = Movement.fuelLevel;
+        if(slider.value <= 0)
+        {
+            sliderBar.color = Color.clear;
+        }
     }
 }
